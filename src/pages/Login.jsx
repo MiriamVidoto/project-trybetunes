0687ad64b50a20483f4899866cc6c6baa309bf46
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2022-07-04 12:16:18 -0300

    Requisito 1 Crie as rotas necessárias para a aplicação ok

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        login
+      </div>
+    );
+  }
+}
+
+export default Login;
