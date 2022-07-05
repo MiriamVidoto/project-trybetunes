24b04f45e65ade55e8cb2872cf9c090ebb2afed5
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2022-07-05 17:52:48 -0300

    requisito 7 ok

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,14 +1,65 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
 
 class Album extends React.Component {
+  state = {
+    arrayMusics: [],
+    dataAlbum: '',
+    dataArtist: '',
+  }
+
+  componentDidMount() {
+    this.fetchMusics();
+  }
+
+  fetchMusics = async () => {
+    const { match } = this.props;
+    const { id } = match.params;
+    const musics = await getMusics(id);
+    this.setState({
+      arrayMusics: musics,
+      dataAlbum: musics[0].collectionName,
+      dataArtist: musics[0].artistName,
+    });
+  }
+
   render() {
+    const { arrayMusics, dataAlbum, dataArtist } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        <div>
+          <div>
+            <h4 data-testid="album-name">{ dataAlbum }</h4>
+            <p data-testid="artist-name">{ dataArtist }</p>
+          </div>
+          {
+            arrayMusics
+              .filter((musics, index) => index !== 0)
+              .map(({ trackName, previewUrl }, i) => (
+                <div key={ i }>
+                  <MusicCard
+                    trackName={ trackName }
+                    previewUrl={ previewUrl }
+                  />
+                </div>
+              ))
+          }
+        </div>
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default Album;
